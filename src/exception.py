1e2562d192f6a6a4676411807c77a664fecb309f
mngembu <mary.ebob@yahoo.ca>
2023-04-10 11:21:38 -0600

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):   #the error_detail will be present in the sys module
     _,_,exc_tb=error_detail.exc_info()
