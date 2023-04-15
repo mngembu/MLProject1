fd3d94e9fb236cbcd9fa27ad424ac66e8d8502f6
mngembu <mary.ebob@yahoo.ca>
2023-04-15 04:08:18 -0600

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass  #used to create class variables
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass                         #used @dataclass here since we are only defining variables, in this case, paths where the trai, test and raw data will be saved
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")    # artifacts is a folder that will be created further down below
@@ -47,4 +50,7 @@ class DataIngestion:            #used __init__ constructor here since we're not 
 
 if __name__ =="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
